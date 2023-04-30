0f922a3f1c091dc7db3dd388faefc01223e51822
ItayDosik <itaydo@ac.sce.ac.il>
2023-04-30 15:43:28 +0300

    Added viewmodel

---
+++ GradeBook/Converters/DoubleToText.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace GradeBook.Converters
+{
+    public class DoubleToText : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return null;
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            if ((string)value == "")
+                return null;
+            try { return double.Parse((string)value); }
+            catch
+            {
+                return null;
+            }
+
+        }
+    }
+}
